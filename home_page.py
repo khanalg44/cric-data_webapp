a664210eed0765cfedf3ae2557c8b66fa2418300
Ghanashyam Khanal <khanalg44@gmail.com>
2021-03-15 17:12:10 -0400

    fix

--- home_page.py
+++ home_page.py
@@ -11,7 +11,7 @@ def app():
     st.markdown("Thank you for visiting the webpage. You can find a lot of data analysis as well as data visualization on this page. Please choose your format on the left panel to see your data as well as visualize the data.")
 
     image = Image.open('./images/consistency.png')
-    st.image(image, caption='Runs vs the standard deviation (normalized) for all the players in IPL (2008-2020) with a cut off runs 2500. (Number of 30+ scores in the bracket.)')
+    st.image(image, caption='Runs vs the normalized standard deviation for all the players in IPL (2008-2020) with a cut off runs 2500. (Number of 30+ scores in the bracket.)')
     Footer()
 
 
