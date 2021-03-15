3b23cfab7c1d63a37d62df7be096192fc105708a
Ghanashyam Khanal <khanalg44@gmail.com>
2021-03-15 18:39:59 -0400

    m

--- home_page.py
+++ home_page.py
@@ -8,7 +8,7 @@ from footer import Footer
 def app():
 
     st.title("Welcome to Cric-Data!")
-    st.markdown("Thank you for visiting the webpage. You can find a lot of data analysis as well as data visualization on this page. Please choose your format on the left panel to see your data as well as visualize the data.")
+    st.markdown("Thank you for visiting the webpage. You can find a lot of data analysis as well as data visualization on this page. Please choose your format on the left panel to see your data as well as visualize the data. Try IPL for a player's profile")
 
     image = Image.open('./images/consistency.png')
     st.image(image, caption='Runs vs the normalized standard deviation for all the players in IPL (2008-2020) with a cut off runs 2500. (Number of 30+ scores in the bracket.)')
